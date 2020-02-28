347deca88187c0ef7b3d04e598e37e319de1ca2f
Stephan Puchegger <stephan.puchegger@univie.ac.at>
2020-02-28 15:24:17 +0100

    Initial commit
    Intial Commit to showcase what I think is an issue in MahApps Metro. *fingers crossed*

---
+++ MahAppsDialog/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+using MahApps.Metro.Controls.Dialogs;
+
+namespace MahAppsDialog {
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application {
+    protected override void OnStartup(StartupEventArgs e) {
+      if (e is null)
+        throw new ArgumentNullException(nameof(e));
+
+      base.OnStartup(e);
+
+      var dialog = new CustomDialog() { Title = "This dialog allows arbitrary content." };
+      dialog.ShowDialogExternally();
+    }
+  }
+}
